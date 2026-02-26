681b377c5a848db3b9e9db3af3d18a48d3272007
Lahmar22 <lahmarzakariae22@gmail.com>
2026-02-26 15:04:33 +0000

    feat : update app for bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->web(\App\Http\Middleware\HandleInvitationToken::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
