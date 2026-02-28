5c21a471734e643d8fc0142254a29e958140aaa6
Lahmar22 <lahmarzakariae22@gmail.com>
2026-02-28 22:49:23 +0000

    feat : update routes

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,20 @@ use App\Http\Controllers\ColocationController;
 use App\Http\Controllers\InvitationController;
 use App\Http\Controllers\expensesController;
 use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdministrateurController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
 
 Route::post('/login', [LoginController::class, 'login']);
 
@@ -25,6 +34,10 @@ Route::middleware('auth')->group(function () {
     Route::post('/user/payments/{payment}/mark-paid', [PaymentController::class, 'markPaid'])->name('payments.markPaid');
     Route::post('/addCategory', [expensesController::class, 'addCategory'])->name('addCategory');
     Route::post('/addExpense', [expensesController::class, 'addExpense'])->name('addExpense');
+    Route::get('/user/profile', [ProfileController::class, 'index'])->name('user.profile');
+    Route::post('/user/profile', [ProfileController::class, 'update'])->name('user.profile.update');
+    Route::post('/user/profile/password', [ProfileController::class, 'updatePassword'])->name('user.profile.password');
+    Route::post('/colocation/quitter', [ColocationController::class, 'quitter'])->name('colocation.quitter');
     
 });
 
@@ -36,9 +49,9 @@ Route::post('/refuse-invitation', [InvitationController::class, 'refuse'])->name
 
 
 Route::middleware('auth')->group(function () {
-    Route::get('/administrateur/dashboard', function () {
-        return view('administrateur.dashboard'); 
-    })->name('administrateur.dashboard');
+    Route::get('/administrateur/dashboard', [AdministrateurController::class, 'dashboard'])->name('administrateur.dashboard');
+    Route::post('/administrateur/ban/{userId}', [AdministrateurController::class, 'banUser'])->name('administrateur.ban');
+    Route::post('/administrateur/unban/{userId}', [AdministrateurController::class, 'unbanUser'])->name('administrateur.unban');
 });
 
 
