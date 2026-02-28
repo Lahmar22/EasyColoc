8806339fa9a2b0ac72702f8ebd03073200370967
Lahmar22 <lahmarzakariae22@gmail.com>
2026-02-28 15:21:00 +0000

    feat : update routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\ColocationController;
 use App\Http\Controllers\InvitationController;
+use App\Http\Controllers\expensesController;
+use App\Http\Controllers\PaymentController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,6 +19,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/user/dashboard', [ColocationController::class, 'index'])->name('user.dashboard');
     Route::post('createColocation', [ColocationController::class, 'create'])->name('createColocation');
     Route::post('/invitationBymail', [InvitationController::class, 'sendEmail'])->name('invitationBymail');
+    Route::post('/joinColocation', [ColocationController::class, 'join'])->name('colocation.join');
+    Route::get('/user/expenses', [expensesController::class, 'index'])->name('user.expenses');
+    Route::get('/user/payments', [PaymentController::class, 'index'])->name('user.payments');
+    Route::post('/user/payments/{payment}/mark-paid', [PaymentController::class, 'markPaid'])->name('payments.markPaid');
+    Route::post('/addCategory', [expensesController::class, 'addCategory'])->name('addCategory');
+    Route::post('/addExpense', [expensesController::class, 'addExpense'])->name('addExpense');
     
 });
 
